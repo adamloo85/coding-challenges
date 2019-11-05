669f373fd74395bebf1169dc28fdbb1c2f71f64b
adamloo85 <adamloo85@gmail.com>
2019-11-04 16:32:53 -0800

    Finished word-search solution

--- wordsearch/solution.js
+++ wordsearch/solution.js
@@ -5,70 +5,84 @@ const board = [
     ["s", "y", "m", "f"]
   ];
   
-  function wordExists(board, letters, rowIdx = 0, colIdx = 0) {
+function wordExists(board, letters, rowIdx = false, colIdx = false) {
     // all of the letters have been found
     if (letters.length === 0) {
-      return true;
+        return true;
     }
-  
+
     // this indicates the first iteration
-    if (rowIdx === 0 && colIdx === 0) {
-      // find a starting index
-      board.forEach((row, rIdx) => {
+    if (rowIdx === false && colIdx === false) {
+        // find a starting index
+        const indices = board.reduce((indices, row, rIdx) => {
+        if (indices && indices.length) {
+            return indices;
+        }
         const cIdx = row.findIndex(letter => letters[0] === letter);
-  
+
         // a match
         if (cIdx !== -1) {
-          return wordExists(board, letters, rIdx, cIdx);
+            return [rIdx, cIdx];
         }
-      });
+        return indices;
+        }, []);
+
+        if (!indices.length) {
+        return false;
+        } else {
+        return wordExists(
+            board,
+            letters.slice(1, letters.length - 1),
+            indices[0],
+            indices[1]
+        );
+        }
     }
     const nextLetter = letters[0];
+    const newLetters = letters.slice(1, letters.length - 1);
     // right
     if (board[rowIdx][colIdx + 1] === nextLetter) {
-      return wordExists(board, letters, rowIdx, colIdx + 1);
+        return wordExists(board, newLetters, rowIdx, colIdx + 1);
     }
-  
+
     // bottom-right
     if (board[rowIdx + 1] && board[rowIdx + 1][colIdx + 1] === nextLetter) {
-      return wordExists(board, letters, rowIdx + 1, colIdx + 1);
+        return wordExists(board, newLetters, rowIdx + 1, colIdx + 1);
     }
-  
+
     // bottom
     if (board[rowIdx + 1] && board[rowIdx + 1][colIdx] === nextLetter) {
-      return wordExists(board, letters, rowIdx + 1, colIdx);
+        return wordExists(board, newLetters, rowIdx + 1, colIdx);
     }
-  
+
     // bottom-left
     if (board[rowIdx + 1] && board[rowIdx + 1][colIdx - 1] === nextLetter) {
-      return wordExists(board, letters, rowIdx + 1, colIdx - 1);
+        return wordExists(board, newLetters, rowIdx + 1, colIdx - 1);
     }
-  
+
     // left
     if (board[rowIdx][colIdx - 1] === nextLetter) {
-      return wordExists(board, letters, rowIdx, colIdx - 1);
+        return wordExists(board, newLetters, rowIdx, colIdx - 1);
     }
-  
+
     // top-left
     if (board[rowIdx - 1] && board[rowIdx - 1][colIdx - 1] === nextLetter) {
-      return wordExists(board, letters, rowIdx - 1, colIdx - 1);
+        return wordExists(board, newLetters, rowIdx - 1, colIdx - 1);
     }
-  
+
     // top
     if (board[rowIdx - 1] && board[rowIdx - 1][colIdx] === nextLetter) {
-      return wordExists(board, letters, rowIdx - 1, colIdx);
+        return wordExists(board, newLetters, rowIdx - 1, colIdx);
     }
-  
+
     // top-right
     if (board[rowIdx - 1] && board[rowIdx - 1][colIdx + 1] === nextLetter) {
-      return wordExists(board, letters, rowIdx - 1, colIdx + 1);
+        return wordExists(board, newLetters, rowIdx - 1, colIdx + 1);
     }
-  
+
     return false;
-  }
-  
-  const word = "camox";
-  
-  // console.log(wordExists(board, "off") === true);
-  console.log(wordExists(board, word.split("")) === true);
-  
+}
+
+console.log(wordExists(board, "off") === true);
+console.log(wordExists(board, "camox") === true);
+console.log(wordExists(board, "doodoo") === false);
